e48b45e1901248a848e71766b12ee1068cb6d3c4
glenm@XPS <glenmuthokaq@gmail.com>
2024-06-22 04:41:24 +0100

    Added AbuseIPDb reporting, using ristretto

--- main.go
+++ main.go
@@ -22,11 +22,14 @@ import (
 	"endlessh-go/metrics"
 	"flag"
 	"fmt"
+	"io"
 	"net"
+	"net/http"
 	"os"
 	"strings"
 	"time"
 
+	"github.com/dgraph-io/ristretto"
 	"github.com/golang/glog"
 )
 
@@ -67,7 +70,7 @@ func startSending(maxClients int64, bannerMaxLength int64, records chan<- metric
 	return clients
 }
 
-func startAccepting(maxClients int64, connType, connHost, connPort string, interval time.Duration, clients chan<- *client.Client, records chan<- metrics.RecordEntry) {
+func startAccepting(maxClients int64, connType, connHost, connPort string, interval time.Duration, clients chan<- *client.Client, records chan<- metrics.RecordEntry, abuseipdbeEnabled bool, abuseIpdbApiKey string) {
 	go func() {
 		l, err := net.Listen(connType, connHost+":"+connPort)
 		if err != nil {
@@ -92,8 +95,111 @@ func startAccepting(maxClients int64, connType, connHost, connPort string, inter
 				LocalPort:  connPort,
 			}
 			clients <- c
+			go reportIPToAbuseIPDB(remoteIpAddr, records, abuseipdbeEnabled, abuseIpdbApiKey)
 		}
 	}()
+}
+
+func reportIPToAbuseIPDB(ip string, records chan<- metrics.RecordEntry, abuseipdbeEnabled bool, abuseIpdbApiKey string) {
+	if !abuseipdbeEnabled {
+		return
+	}
+	if isCached(ip) {
+		glog.V(1).Infof("IP is already cached, skipping report")
+		records <- metrics.RecordEntry{
+			RecordType: metrics.RecordEntryTypeReport,
+			IpAddr:     ip,
+			Message:    "IP is already cached, skipping report",
+		}
+		return
+	}
+	appendToReportedIPs(ip) // Cache the IP before possibly early exiting due to API key issues
+
+	var apiKey string
+	if abuseIpdbApiKey != "" {
+		apiKey = abuseIpdbApiKey
+	} else {
+		glog.V(1).Infof("AbuseIPDB API key not set, skipping report")
+		records <- metrics.RecordEntry{
+			RecordType: metrics.RecordEntryTypeReport,
+			IpAddr:     ip,
+			Message:    "AbuseIPDB API key not set, skipping report",
+		}
+		return
+	}
+
+	// Format the timestamp in ISO 8601 format, including the timezone (Z for UTC)
+	timestamp := time.Now().UTC().Format(time.RFC3339)
+	reportURL := fmt.Sprintf("https://api.abuseipdb.com/api/v2/report?ip=%s&categories=18,22&comment=SSH honeypot connection attempt.&timestamp=%s", ip, timestamp)
+	req, err := http.NewRequest("POST", reportURL, nil)
+	if err != nil {
+		glog.V(1).Infof("Error creating request: %v", err)
+		records <- metrics.RecordEntry{
+			RecordType: metrics.RecordEntryTypeReport,
+			IpAddr:     ip,
+			Message:    fmt.Sprintf("Error creating request: %v", err),
+		}
+		return
+	}
+
+	req.Header.Set("Key", apiKey)
+	req.Header.Set("Accept", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		records <- metrics.RecordEntry{
+			RecordType: metrics.RecordEntryTypeReport,
+			IpAddr:     ip,
+			Message:    fmt.Sprintf("Error making request: %v", err),
+		}
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusUnprocessableEntity {
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			glog.V(1).Infof("Error reading response body: %v", err)
+		} else {
+			glog.V(1).Infof("Unprocessable Entity response from AbuseIPDB: %s", body)
+		}
+		records <- metrics.RecordEntry{
+			RecordType: metrics.RecordEntryTypeReport,
+			IpAddr:     ip,
+			Message:    fmt.Sprintf("Unprocessable Entity response from AbuseIPDB: %s", resp.Status),
+		}
+		return
+	}
+
+	glog.V(1).Infof("Reported IP to AbuseIPDB: %s", resp.Status)
+	records <- metrics.RecordEntry{
+		RecordType: metrics.RecordEntryTypeReport,
+		IpAddr:     ip,
+		Message:    fmt.Sprintf("Reported IP to AbuseIPDB: %s", resp.Status),
+	}
+}
+
+func setupCache() {
+	config := &ristretto.Config{
+		NumCounters: 1e7,     // NumCounters is 10x the number of items you expect to keep in the cache when full.
+		MaxCost:     1 << 30, // Maximum cost of cache (e.g., bytes if the cost is measured in bytes).
+		BufferItems: 64,      // Number of keys per Get buffer.
+	}
+
+	var err error
+	cache, err = ristretto.NewCache(config)
+	if err != nil {
+		glog.Fatalf("Failed to create cache: %v", err)
+	}
+}
+
+func isCached(ip string) bool {
+	_, found := cache.Get(ip)
+	return found
+}
+
+func appendToReportedIPs(ip string) {
+	cache.Set(ip, struct{}{}, 1)
 }
 
 type arrayStrings []string
@@ -109,15 +215,19 @@ func (a *arrayStrings) Set(value string) error {
 
 const defaultPort = "2222"
 
+var cache *ristretto.Cache
 var connPorts arrayStrings
 
 func main() {
+	setupCache()
 	intervalMs := flag.Int("interval_ms", 1000, "Message millisecond delay")
 	bannerMaxLength := flag.Int64("line_length", 32, "Maximum banner line length")
 	maxClients := flag.Int64("max_clients", 4096, "Maximum number of clients")
 	connType := flag.String("conn_type", "tcp", "Connection type. Possible values are tcp, tcp4, tcp6")
 	connHost := flag.String("host", "0.0.0.0", "SSH listening address")
 	flag.Var(&connPorts, "port", fmt.Sprintf("SSH listening port. You may provide multiple -port flags to listen to multiple ports. (default %q)", defaultPort))
+	abuseipdbeEnabled := flag.Bool("enable_abuseipdb", false, "Enable AbuseIPDB reporting")
+	abuseIpdbApiKey := flag.String("abuse_ipdb_api_key", "", "AbuseIPDB API key")
 	prometheusEnabled := flag.Bool("enable_prometheus", false, "Enable prometheus")
 	prometheusHost := flag.String("prometheus_host", "0.0.0.0", "The address for prometheus")
 	prometheusPort := flag.String("prometheus_port", "2112", "The port for prometheus")
@@ -155,7 +265,7 @@ func main() {
 		connPorts = append(connPorts, defaultPort)
 	}
 	for _, connPort := range connPorts {
-		startAccepting(*maxClients, *connType, *connHost, connPort, interval, clients, records)
+		startAccepting(*maxClients, *connType, *connHost, connPort, interval, clients, records, *abuseipdbeEnabled, *abuseIpdbApiKey)
 	}
 	for {
 		if *prometheusCleanUnseenSeconds <= 0 {
