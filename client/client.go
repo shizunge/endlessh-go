0d5395b4ee278fdc7700c8beec261b3493848d9d
Shizun Ge <shizunge@gmail.com>
2026-01-05 20:07:30 -0800

    Update copyright to 2026.

--- client/client.go
+++ client/client.go
@@ -1,4 +1,4 @@
-// Copyright (C) 2021-2024 Shizun Ge
+// Copyright (C) 2021-2026 Shizun Ge
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
